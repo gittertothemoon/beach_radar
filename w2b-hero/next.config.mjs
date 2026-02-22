39b5935236ff057f49d8c2ae5ec2bb84fe88f507
PYONIUMM <ivapanto97@gmail.com>
2026-02-22 19:09:34 +0100

    fix(next.config): remove legacy landing redirects

--- w2b-hero/next.config.mjs
+++ w2b-hero/next.config.mjs
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      {
-        source: "/landing",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/landing/index.html",
-        destination: "/",
-        permanent: true,
-      },
-    ];
+    return [];
   },
 };
 
