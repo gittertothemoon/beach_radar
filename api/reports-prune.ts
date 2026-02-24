c5b1d16063064624d9b584839a8e71b1553d1542
PYONIUMM <ivapanto97@gmail.com>
2026-02-24 21:26:22 +0100

    fix(api): use dedicated file for vercel cron validation

---
+++ api/reports-prune.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import reportsPrune from "./_handlers/reports-prune";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    return reportsPrune(req, res);
+}
